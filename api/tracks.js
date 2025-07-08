4cc51101ceb110710c21d9e8d619fe803edb5efd
Emerson Recto <emersontrecto@gmail.com>
2025-07-07 20:14:00 -0400

    completed app

--- api/tracks.js
+++ api/tracks.js
@@ -3,14 +3,26 @@ const router = express.Router();
 export default router;
 
 import { getTracks, getTrackById } from "#db/queries/tracks";
+import requireUser from "#middleware/requireUser";
+import { getPlaylistsByTrackId } from "#db/queries/playlists";
 
 router.route("/").get(async (req, res) => {
   const tracks = await getTracks();
   res.send(tracks);
 });
 
-router.route("/:id").get(async (req, res) => {
-  const track = await getTrackById(req.params.id);
-  if (!track) return res.status(404).send("Track not found.");
-  res.send(track);
+router.param("id", async (req, res, next, id) => {
+  const track = await getTrackById(id);
+  if (!track) return res.status(404).send("Track does not exist.");
+  req.track = track;
+  next();
+});
+
+router.route("/:id").get((req, res) => {
+  res.send(req.track);
+});
+
+router.route("/:id/playlists").get(requireUser, async (req, res) => {
+  const playlists = await getPlaylistsByTrackId(req.track.id);
+  res.send(playlists);
 });
--- db/queries/playlists.js
+++ db/queries/playlists.js
@@ -14,14 +14,14 @@ export async function createPlaylist(name, description, userId) {
   return playlist;
 }
 
-export async function getPlaylists() {
-  const sql = `
-  SELECT *
-  FROM playlists
-  `;
-  const { rows: playlists } = await db.query(sql);
-  return playlists;
-}
+// export async function getPlaylists() {
+//   const sql = `
+//   SELECT *
+//   FROM playlists
+//   `;
+//   const { rows: playlists } = await db.query(sql);
+//   return playlists;
+// }
 
 export async function getPlaylistById(id) {
   const sql = `
@@ -33,4 +33,26 @@ export async function getPlaylistById(id) {
     rows: [playlist],
   } = await db.query(sql, [id]);
   return playlist;
+}
+
+export async function getPlaylistByUserId(id) {
+  const sql = `
+  SELECT *
+  FROM playlists
+  WHERE user_id = $1
+  `;
+  const { rows: playlists } = await db.query(sql, [id]);
+  return playlists;
+}
+
+export async function getPlaylistsByTrackId(id) {
+  const sql = `
+  SELECT playlists.*
+  FROM
+    playlists
+    JOIN playlists_tracks ON playlists.id = playlists_tracks.playlist_id
+  WHERE playlists_tracks.track_id = $1
+  `;
+  const { rows: playlists } = await db.query(sql, [id]);
+  return playlists;
 }
